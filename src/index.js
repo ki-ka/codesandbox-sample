b73026ec3dd9a8d6492704c40c0035e50aa6db35
ki-ka <100897171+ki-ka@users.noreply.github.com>
2022-03-04 17:09:31 +0900

    udemy(~4-23)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,114 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// //こめんと
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 ="let変数を上書き"
+// console.log(val2);
+
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // val3 = "const変数を上書き";
+
+// const val3 = "const変数を再宣言";
+
+//
+// const val4 = {
+//   name: "じゃけぇ",
+//   age : 28,
+// }
+// val4.name = "jak";
+// val4.addres = "Hiroshima";
+// console.log(val4);
+
+//
+// const val5 = ["dog", "cat"];
+// val5[0] = "pig";
+// val5.push("monkey");
+// console.log(val5);
+
+// const name = "じゃけぇ";
+// const age = 28;
+
+// //じゅうらいのほうほう
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+// console.log(message1);
+
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// function func1(String){
+//   return String;
+// }
+// const func1 = function (String) {
+//   return String;
+// };
+// console.log(func1("func1です"));
+
+// //アロー関数
+// const func2 = (String) => String;
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// console.log(func3(10, 20));
+
+// const myProfile = {
+//   name : "ki",
+//   age : 28,
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const {name, age} = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// const myProfile = [`じゃけぇ`, 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
+// sayHello("じゃけぇ");
+// const arr1 = [1, 2];
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
